854eb8d25d6d673782c4ce90e72eb9a05cb43b19
550185271 <550185271@qq.com>
2020-07-20 14:49:19 +0800

    feat: 子组件props、context参数；

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,27 @@
 const { reactive } = Vue
-var App = {
+let Child = {
+  // 属性定义
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  template: `<div>{{title}}</div>`,
+  setup(props, context) {
+    console.log(props)
+    console.log(context)
+  }
+}
+let App = {
   template: `
     <div class="container">
          {{message}}
          <input v-model="name" />
          <p>{{name}}</p>
+         <Child title="test props two arguments"/>
     </div>`,
+  components: { Child },
   setup() {
     const state = reactive({
       message: 'Hello World!!!',
